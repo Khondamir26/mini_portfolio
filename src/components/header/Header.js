b22d27f533a2c9cd2368504a60e3dd70deb99000
Khondamir26 <82398768+Khondamir26@users.noreply.github.com>
2024-08-30 03:10:30 +0500

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
 				<div className="header__text">
 					<p>with passion for learning and creating.</p>
 				</div>
-				<a href="" className="btn">
+				<a href="https://cvkhonda.netlify.app/" className="btn">
 					Download CV
 				</a>
 			</div>
